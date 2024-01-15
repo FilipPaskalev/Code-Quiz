696bee7ef8638bb50e156c36dbd76d63561feddd
FilipPaskalev <77507389+FilipPaskalev@users.noreply.github.com>
2024-01-15 22:00:38 +0000

    create obj for questions

---
+++ assets/js/questions.js
@@ -0,0 +1,17 @@
+const questions = [
+  {
+    questionTitle: "question 1",
+    choices: ["K 1", "choice 2", "choice 3", "choice 4"],
+    answer: "K 1",
+  },
+  {
+    questionTitle: "question 2",
+    choices: ["D 1", "choice 2", "choice 3", "choice 4"],
+    answer: "D 2",
+  },
+  {
+    questionTitle: "question 3",
+    choices: ["A 1", "choice 2", "choice 3", "choice 4"],
+    answer: "A 1",
+  },
+];
