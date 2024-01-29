3f23a1c977b35e2b111987e1233be565ceb4ff8b
FilipPaskalev <77507389+FilipPaskalev@users.noreply.github.com>
2024-01-29 16:49:22 +0000

    refactor elements name in startQuiz()

--- js/logic.js
+++ js/logic.js
@@ -4,8 +4,8 @@ let questionIndex = 0;
 let score = 0;
 
 function startQuiz() {
-  startScreenElement.setAttribute("class", "hide");
-  questionsElement.removeAttribute("class");
+  startScrnEl.setAttribute("class", "hide");
+  questionsEl.removeAttribute("class");
 
   timerInterval = setInterval(() => {
     time = time - 1;
@@ -18,51 +18,51 @@ function startQuiz() {
   renderQuestion();
 }
 
-function renderQuestion() {
-  var currentQuestion = questions[questionIndex];
-  // Set title
-  questionTitleEl.textContent = currentQuestion.question;
+// function renderQuestion() {
+//   var currentQuestion = questions[questionIndex];
+//   // Set title
+//   questionTitleEl.textContent = currentQuestion.question;
 
-  questionsElement.appendChild();
-  console.log("🚀 ~ renderQuestion ~ currentQuestion:", currentQuestion);
-}
+//   questionsElement.appendChild();
+//   console.log("🚀 ~ renderQuestion ~ currentQuestion:", currentQuestion);
+// }
 
-function endQuiz() {
-  clearInterval(timerInterval);
-  questionsElement.setAttribute("class", "hide");
-  endScreenElement.removeAttribute("class");
-}
+// function endQuiz() {
+//   clearInterval(timerInterval);
+//   questionsElement.setAttribute("class", "hide");
+//   endScreenElement.removeAttribute("class");
+// }
 
-function generateRandomQuestion() {
-  var randomQuestion = {};
-  var randomIndex = (randomQuestion =
-    questions[keys[(keys.length * Math.random()) << 0]]);
-  deleteElementFromArrayByIndex(randomIndex);
-  return randomQuestion;
-}
+// function generateRandomQuestion() {
+//   var randomQuestion = {};
+//   var randomIndex = (randomQuestion =
+//     questions[keys[(keys.length * Math.random()) << 0]]);
+//   deleteElementFromArrayByIndex(randomIndex);
+//   return randomQuestion;
+// }
 
-function deleteElementFromArrayByIndex(index) {
-  delete questions[index];
-}
+// function deleteElementFromArrayByIndex(index) {
+//   delete questions[index];
+// }
 
-function checkAnswer(answer, userChoice) {
-  if (userChoice === answer) {
-    return "Correct!";
-  } else {
-    return "Wrong!";
-  }
-}
+// function checkAnswer(answer, userChoice) {
+//   if (userChoice === answer) {
+//     return "Correct!";
+//   } else {
+//     return "Wrong!";
+//   }
+// }
 
-function startTiming() {
-  if (parseInt(time.textContent) < 75) {
-    time.textContent = parseInt(time.textContent) + 1;
-    console.log(time.textContent);
-  } else {
-    console.log("clear interval");
-    clearInterval();
-  }
-}
+// function startTiming() {
+//   if (parseInt(time.textContent) < 75) {
+//     time.textContent = parseInt(time.textContent) + 1;
+//     console.log(time.textContent);
+//   } else {
+//     console.log("clear interval");
+//     clearInterval();
+//   }
+// }
 
-startBtn.onclick = startQuiz;
+// startBtn.onclick = startQuiz;
 
-setInterval(startTiming, 1000);
+// setInterval(startTiming, 1000);
