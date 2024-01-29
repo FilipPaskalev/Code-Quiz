154f25d1b0fbd0375a0d3939a190e1d42e5836d5
FilipPaskalev <77507389+FilipPaskalev@users.noreply.github.com>
2024-01-29 16:30:11 +0000

    add separate file for utils for index.html and highscores.html

---
+++ js/utils/highscores.js
@@ -0,0 +1,7 @@
+// TODO add JSDoc description of the file
+
+// Lists
+const highScoresOl = document.getElementById("highscores");
+
+// Buttons
+const clearBtn = document.getElementById("clear");
--- js/utils/index.js
+++ js/utils/index.js
@@ -2,22 +2,21 @@
 
 // Elements
 const timeEl = document.getElementById("time");
-const feedbackEl = document.getElementById("feedback");
 const startScrnEl = document.getElementById("start-screen");
-const endScrnEl = document.getElementById("end-screen");
 const questionsEl = document.getElementById("questions");
 const questionTitleEl = document.getElementById("question-title");
 const choicesEl = document.getElementById("choices");
+const endScrnEl = document.getElementById("end-screen");
 const finalScoreEl = document.getElementById("final-score");
+const feedbackEl = document.getElementById("feedback");
+
+// Inputs
+const initialsInput = document.getElementById("initials");
 
 // Sounds
 const incorrectSound = new Audio("assets/sfx/incorrect.wav");
 const correctSound = new Audio("assets/sfx/correct.wav");
 
-// Lists
-const highScoresLi = document.getElementById("highscores");
-
 // Buttons
-const submitBtn = document.getElementById("submit");
 const startBtn = document.getElementById("start");
-const clearBtn = document.getElementById("clear");
+const submitBtn = document.getElementById("submit");
