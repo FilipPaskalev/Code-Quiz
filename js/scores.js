ae5a00ca6f9edd0edb18262b56042be9f1e75099
FilipPaskalev <77507389+FilipPaskalev@users.noreply.github.com>
2024-01-29 20:06:25 +0000

    add needed functions for scores.js (not added logic to the yet)

--- js/scores.js
+++ js/scores.js
@@ -1,3 +1,11 @@
 // TODO add JSDoc description of the file - what contains, what is the purpose of the file and so on
 
-var playerScore = 0;
+function renderScores() {}
+
+function clearScores() {}
+
+function loadScores() {}
+
+renderScores();
+
+clearHighScoresBtn.addEventListener("click", clearScores);
